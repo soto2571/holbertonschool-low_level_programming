1e935db7ff6b08681a3d39d12a9e4764705f2063
soto2571 <sebastian.soto1649@gmail.com>
2024-02-19 11:29:12 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -o 4-puts.c |  const char *str = "Programming is like building a multilingual puzzle"
+#include <stdio.h>
+
+int main() {
+
+const char *str = "Programming is like building a multilingual puzzle";
+printf("%s",str);
+
+
+
+
+
+
+return 0;
+}
