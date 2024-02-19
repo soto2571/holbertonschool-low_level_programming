604e7a5399eb802f2b1e65372ef817d05c7ddc77
soto2571 <sebastian.soto1649@gmail.com>
2024-02-19 14:22:03 -0400

    qwerty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 char charType;
 int intType;
 long int longintType;
-long long longlongType;
+long long int longlongintType;
 float floatType;
 
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
-printf("Size of int: %zu byte(s)\n", sizeof(intType));
-printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of long long: %zu byte(s)\n", sizeof(longlongType));
-printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
