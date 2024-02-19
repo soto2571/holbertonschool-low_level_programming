b6ce8e6b78f43538e4f9f4cc72673a6c15170891
soto2571 <sebastian.soto1649@gmail.com>
2024-02-19 13:03:18 -0400

    oiu

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
+/*
+ *puts- Print the message we were ask.
+ *
+ *Description:This function prints the desire message.
+ *Return:If success it will print the message and if not it will error.
+ */
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
