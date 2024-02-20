819345bcc9f57fff107d438407dd5a2ac88ca799
soto2571 <sebastian.soto1649@gmail.com>
2024-02-20 11:39:06 -0400

    tyv

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,9 +19,9 @@ int main(void)
 	}
 	else if (n < 6)
 	{
-		printf("Last digit of %d and is less than 6 and not 0 \n",n);
+		printf("Last digit of %d and is less than 6 and not 0 \n", n);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d and is 0 \n", n);
 	}
