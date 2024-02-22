a7f69843c0c3c3cf5b9183cf5acb3852b36948a8
soto2571 <sebastian.soto1649@gmail.com>
2024-02-22 12:04:43 -0400

    worio

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,8 @@
 /**
  *print_alphabet_x10 - Start of the program
  *
+ *Description:Printing the alphabet ten times
+ *
  *Return:Will be void
  */
 void print_alphabet_x10(void)
@@ -9,10 +11,12 @@ void print_alphabet_x10(void)
 int num;
 char abc;
 
-for (num = 0; num <= 10; num++)
+for (num = 0; num < 10; num++)
 {
 for (abc = 'a'; abc <= 'z'; abc++)
+{
 	_putchar(abc);
+}
 	_putchar('\n');
 }
 
