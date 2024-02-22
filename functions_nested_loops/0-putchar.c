107ea3b4fd03adcf52e414e35977b1d7e0665da7
soto2571 <sebastian.soto1649@gmail.com>
2024-02-22 08:56:32 -0400

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- *main - Start of the program
- *
- *Return:0
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-printf("_putchar");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-return (0);
+	return (0);
 }
