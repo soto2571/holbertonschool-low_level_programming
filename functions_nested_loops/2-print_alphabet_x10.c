16a83ec833ca816a45d8601e101e8ed0bd348062
soto2571 <sebastian.soto1649@gmail.com>
2024-02-22 11:53:32 -0400

    anuel brr

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#inlcude "main.h"
+/**
+ *print_alphabet_x10 - Start of the program
+ *
+ *Return:Will be void
+ */
+void print_alphabet_x10(void)
+{
+int num;
+char abc;
+
+for (num = 0; num <= 10; num++)
+{
+for (abc = 'a'; abc <= 'z'; abc++)
+	_putchar(abc);
+	_putchar('\n');
+}
+
+
