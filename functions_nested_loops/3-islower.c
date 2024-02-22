b8cfc7d1152ca673b39ab84972df794f9393e862
soto2571 <sebastian.soto1649@gmail.com>
2024-02-22 12:58:36 -0400

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - Start of the program
+ *@c:Check for lowecase
+ *Return:0
+ *
+ */
+int _islower(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+
+return (0);
+}
