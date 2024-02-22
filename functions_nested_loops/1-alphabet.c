c36bcc0e4faeeedb1b0e78dbaf75f955b979e1b7
soto2571 <sebastian.soto1649@gmail.com>
2024-02-22 11:11:48 -0400

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - Start of the program
+ *
+ *Return:0
+ *
+ */
+int alphabet(void)
+{
+char abc = 'a';
+
+for (abc = 'a'; abc <= 'z'; abc++)
+	_putchar(abc);
+	_putchar('\n');
+
+print_alphabet();
+return (0);
+}
