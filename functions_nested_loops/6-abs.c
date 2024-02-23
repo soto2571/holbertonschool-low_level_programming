d7c88e9961a525db66c2f21b312e22fd31479f5f
soto2571 <sebastian.soto1649@gmail.com>
2024-02-23 09:27:18 -0400

    hsafjjk

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Start of the program
+ *@r: Give absolute value
+ * Return:0
+ */
+int _abs(int r)
+{
+if (r < 0)
+{
+	return (-r);
+}
+else
+{
+	return (r);
+}
+
+return (0);
+}
