ad2e35909ad91c7de875527a7704ffb42bd2a533
soto2571 <sebastian.soto1649@gmail.com>
2024-02-23 09:53:27 -0400

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_last_digit - Start of the program
+ *@r:Print last digit of the number
+ *Return:0
+ */
+int print_last_digit(int r)
+{
+int lastDigit = r % 10;
+_putchar ('0' + lastDigit);
+_putchar ('\n');
+
+return (0);
+}
