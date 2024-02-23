de9038ea77eabbf7d8de794fffbd71ada2d950a1
soto2571 <sebastian.soto1649@gmail.com>
2024-02-23 13:15:19 -0400

    sjkfs

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - Start of the program
+ *@a:first integer
+ *@b:second integer
+ *Return:0
+ */
+int add(int a, int b)
+{
+int sum;
+
+sum = (a + b);
+
+return (sum);
+}
