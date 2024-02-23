3e9f78fde3d73674b5137826fe6cb3e917e3d510
soto2571 <sebastian.soto1649@gmail.com>
2024-02-23 13:35:28 -0400

    jlsf

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,20 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 /**
- *
- *
- *
+ *print_to_98 - Start of the prgoram
+ *@n:number
+ *Return:0
  */
 void print_to_98(int n)
 {
+int;
 
-
-
-
+for (n >= 1; n <= 98; n++)
+{
+	_putchar(',');
+	_putchar(' ');
+}
+	_putchar(n);
 
 return (0);
 }
