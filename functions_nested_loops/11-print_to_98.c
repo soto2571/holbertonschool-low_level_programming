2665a4590b737b251558726d603242f3674abc9d
soto2571 <sebastian.soto1649@gmail.com>
2024-02-24 08:31:06 -0600

    flkelfk

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,36 @@
  */
 void print_to_98(int n)
 {
-
-if (n == 0)
-{
-for ( ; n <= 98; n++)
-{
-	printf("%d", n);
-	printf(",");
-	printf(" " );
-}
-}
-if (n > 0)
-{
-for ( ; n <= 98; n++)
-{
-	printf("%d", n);
-	printf(",");
-	printf(" ");
-}
-}
-if (n < 0)
-{
-for ( ; n <=98; n--)
-{
-	printf("%d", n);
-	printf(",");
-	printf(" ");
-}
-}
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
 }
