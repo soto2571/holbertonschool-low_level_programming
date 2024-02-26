85fb772c28987a219822d430a5c4baa5ffe4cf17
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 10:08:00 -0400

    no se

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - Start of the program
+ *@a: Is a number
+ *@b: Is another number
+ *Return:0
+ */
+int mul(int a, int b)
+{
+int x = a * b;
+
+	_putchar('x');
+
+return (0);
+}
