f0bd63f52cceca3060614216eaeec3c47d1fb063
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 10:27:27 -0400

    skffjs

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - Start of the program
+ *
+ *Description:Print numbers
+ *
+ *Return:0
+ */
+void print_numbers(void)
+{
+int num;
+
+if (num >= 48 && num <= 57)
+{
+	_putchar(num);
+	_putchar('\n');
+
+}
