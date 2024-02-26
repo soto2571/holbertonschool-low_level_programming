3bb921339e7126e8626b9d51f2d47534881b984e
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 14:02:50 -0400

    flkfkjf

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_diagonal - Start of the program
+ *@n: Draws in diagonal
+ *Return:0
+ */
+void print_diagonal(int n)
+{
+int i;
+
+char slash = '\n';
+
+char line = '92';
+
+for (i = 1; i <= n; i++)
+{
+	_putchar(line);
+
+	if (n <= 0)
+	{
+	_putchar(slash);
+	}
+}
+_putchar('\n');
+}
+
+
+
+
+
+}
