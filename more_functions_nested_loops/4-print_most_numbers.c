6fdd293a63473bc940eb5b7f8ab76146eb86fa8c
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 11:08:03 -0400

    era eso

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_most_numbers - Start of the program
+ *Description:Print almost all numbers
+ */
+void print_most_numbers(void)
+{
+int x;
+
+for (x >= 0; x <= 9; x++)
+{
+	if (x != 2 || x != 4)
+	{
+		_putchar(x + '0');
+	}
+}
+_putchar('\n');
+
+}
