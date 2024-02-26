0d6a5ff26b77d9c14114508d3b3d4ceddeb26adc
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 11:24:23 -0400

    creo que si

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#inlcude "main.h"
+/**
+ *more_numbers - Start of the program
+ *
+ *Description:10 times the numbers
+ */
+void more_numbers(void)
+{
+int x;
+int y;
+
+for (x = 0; x <= 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
+}
