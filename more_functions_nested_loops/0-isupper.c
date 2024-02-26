a367c9cee9cb783625eef5d5f39678fe95fe9b63
soto2571 <sebastian.soto1649@gmail.com>
2024-02-26 09:41:46 -0400

    sfljf

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isupper - Start of the program
+ *@c: Check for uppercase
+ *Return:0
+ */
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'z')
+{
+	return (1);
+}
+return (0);
+}
