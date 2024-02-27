9fea886e00eb57b03bedc7e13e105751da291a7c
soto2571 <sebastian.soto1649@gmail.com>
2024-02-27 13:52:53 -0400

    kfwejkf

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - Start of the program
+ *Description: swap values
+ *@a:Value a
+ *@b:Value b
+ *Return:0
+ */
+void swap_int(int *a, int *b)
+{
+int *a;
+int *b;
+
+int a = b;
+
+
+}
