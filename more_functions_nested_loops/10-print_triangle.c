a821842d19b2d71d732d03a808f983d108d78a87
soto2571 <sebastian.soto1649@gmail.com>
2024-02-27 11:26:42 -0400

    sdkljfjf

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,26 +7,26 @@
  */
 void print_triangle(int size)
 {
-    int x;
-    int y;
+int x;
+int y;
 
-    if (size > 0)
-    {
-        for (x = 1; x <= size; x++)
-        {
-            for (y = 1; y <= size; y++)
-            {
-                if (y <= size - x)
-                    _putchar(' ');
-                else
-                    _putchar('#');
-            }
+if (size > 0)
+	{
+	for (x = 1; x <= size; x++)
+	{
+	for (y = 1; y <= size; y++)
+	{
+	if (y <= size - x)
+	_putchar(' ');
+	else
+	_putchar('#');
+	}
 
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
