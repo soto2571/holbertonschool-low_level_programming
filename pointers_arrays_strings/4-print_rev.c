431c2894c807f2a8125f5a4f60a47883e2d332cd
soto2571 <sebastian.soto1649@gmail.com>
2024-02-27 20:00:14 -0400

    fjkdfkls

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_rev - Start of the program
+ *
+ *@s:Variable for reverse print
+ *
+ */
+void print_rev(char *s)
+{
+if (s != NULL)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
+}
