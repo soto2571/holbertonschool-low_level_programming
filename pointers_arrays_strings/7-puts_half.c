bdc3a8a9a15ed612af69d3c65c6f2eae2271638f
soto2571 <sebastian.soto1649@gmail.com>
2024-02-28 13:57:42 -0400

    ffkjf

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *puts_half - Start of the program
+ *@str: Prints half of the string
+ *
+ */
+void puts_half(char *str)
+{
+int len = 0;
+int i = 0;
+
+while (str[len] != '\0')
+{
+	len++
+}
+while (i < len)
+{
+	_putchar(str[i]);
+	i / 2;
+}
+}
