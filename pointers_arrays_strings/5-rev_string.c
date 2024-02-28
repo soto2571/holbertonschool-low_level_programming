b603b649521204970550b77b69160f99681f63a8
soto2571 <sebastian.soto1649@gmail.com>
2024-02-28 11:21:57 -0400

    fjfkjlsf

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,10 +15,10 @@ while (s[len] != '\0')
 {
 	len++;
 }
-for (i = len -1; i >= len / 2; i--)
+for (i = len - 1; i >= len / 2; i--)
 {
 	temp = s[i];
-	s[i] = s[len -i -1];
-	s[len -i -1] = temp;
+	s[i] = s[len - i - 1];
+	s[len - i - 1] = temp;
 }
 }
