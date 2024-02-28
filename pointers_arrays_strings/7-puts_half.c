8fe83fe84fa51da6b84ae1315dcd70cba7fe8381
soto2571 <sebastian.soto1649@gmail.com>
2024-02-28 14:26:09 -0400

    skljfskl

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,28 +6,28 @@
  */
 void puts_half(char *str)
 {
-int len = 0;
+int len;
 int i = 0;
-int j;
+int half;
 int start_index;
 
 while (str[i] != '\0')
 {
-	len++;
 	i++;
 }
-if (len % 2 == 0)
+
+len = i;
+
+half = (len - 1) / 2;
+
+
+for (start_index = half; start_index <= len - 1; start_index++)
 {
-	start_index = len -1 / 2;
+	if (start_index % 2 == 0)
+	{
+		_putchar(str[start_index]);
+	}
+	_putchar('\n');
 }
-else
-{
-	start_index = (len - 1) / 2;
-}
-for (j = start_index; j < len; j++)
-{
-	_putchar(str[j]);
-}
-_putchar('\n');
 }
 
