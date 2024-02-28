98853132b8622a26929b5cd15f9bb6321c16a0b8
soto2571 <sebastian.soto1649@gmail.com>
2024-02-28 13:37:00 -0400

    sfjsfjk

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -20,5 +20,7 @@ for (i = len - 1; i >= len / 2; i--)
 	temp = s[i];
 	s[i] = s[len - i - 1];
 	s[len - i - 1] = temp;
+	printf("this is with i: %c", s[len - i - 1]);
+	printf("this is with out i : %c", s[len - 1]);
 }
 }
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *puts2 - Start of the program
+ *@str:Prints every other caracter
+ *
+ *
+ */
+void puts2(char *str)
+{
+int len = 0;
+int i = 0;
+for (str[len] != '\0')
+{
+	len++
+}
+for (i = len - 1; i < '\0'; i += 2)
+{
+	_putchar(str[len]);
+}
+}
