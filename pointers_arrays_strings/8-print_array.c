ee610acb11c542b373a41efc7ef2366a05126ba6
soto2571 <sebastian.soto1649@gmail.com>
2024-02-28 14:55:29 -0400

    flkjfkfj

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main."
+#include <stdio.h>
+/**
+ *print_array - Start of the program
+ *@n:Prints elements on array
+ *@a:Have values of array
+ *
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+
+	if (i < n - 1)
+	{
+		printf(", ");
+	}
+}
+printf("\n");
+}
