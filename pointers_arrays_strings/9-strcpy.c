fd4b6cccc1c28f0afe792001cfde67869a160c36
soto2571 <sebastian.soto1649@gmail.com>
2024-02-29 09:50:33 -0400

    fkljfjwf

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strcpy - Start of the program
+ *@dest:Value of the pointer
+ *@src:Another pointer
+ *Return:dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *originalDest = dest;
+
+while ((*dest++ = *src++) != '\0')
+{
+}
+
+return (dest);
+}
