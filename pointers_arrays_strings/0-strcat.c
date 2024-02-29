061c6944e69b718bd7245f6ee230f88b8d7d79b4
soto2571 <sebastian.soto1649@gmail.com>
2024-02-29 19:30:14 -0400

    flkjsfjkls

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcat - Start of the program
+ *
+ *@dest:
+ *
+ *@src:
+ *
+ * Return:dest
+ */
+char *_strcat(char *dest, char *src)
+{
+char string1 = *dest;
+char string2 = *src;
+
+while ((string1++ = string2++) != '\0')
+{
+}
+
+return (string1);
+}
--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,6 @@ do {
 		sign *= -1;
 
 	else if (*s >= '0' && *s <= '9')
-		num = (num * 10) + (*s - '0');
 
 	else if (num > 0)
 		break;
