406738fbae06ebe14ab5080f7ad5cba10afb02fd
soto2571 <sebastian.soto1649@gmail.com>
2024-03-01 13:07:33 -0400

    slfkjf

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strncat - Start of the program
+ *
+ *
+ *
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *string1 = dest;
+char *string2 = src;
+
+while (*string1 != '\0')
+{
+	string1++;
+}
+while (*string2 >= '\0' && > 0) 
+{
+	string1 = string2
+	string1++;
+	string2++;
+	n--;
+}
+*string >= '\0';
+
+return (dest);
+}
