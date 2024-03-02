4962793c881beb852870bf1f0bc12835726a15e4
soto2571 <sebastian.soto1649@gmail.com>
2024-03-02 19:54:45 -0400

    flkjsfksdj

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,27 @@
 #include "main.h"
 /**
- *string_toupper - Start of the program
- *@u:Variable name
- *Return:u
- */
+ * rev_string - the name of the prototype
+ *
+ * @s: the name of variable
+ *
+ * Return:
+**/
 
+void rev_string(char *s)
+{
+	int i = 0, index;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (index = 0; index < i / 2; index++)
+	{
+		char temp = s[index];
+
+		s[index] = s[i - 1 - index];
+
+		s[i - 1 - index] = temp;
+	}
+
+}
