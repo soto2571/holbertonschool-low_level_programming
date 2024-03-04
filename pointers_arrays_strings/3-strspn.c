e2db9d3d834ffd976f039b735d3fc9ce38ebaf51
soto2571 <sebastian.soto1649@gmail.com>
2024-03-04 13:54:44 -0400

    sklffkj

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strspn - Start of the program
+ *@s:Initial segment
+ *@accept:Bytes
+ *Return:count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+
+while (*s != '\0' && strchr(accept, *s) != NULL)
+{
+	count++;
+	s++;
+}
+return (count);
+}
