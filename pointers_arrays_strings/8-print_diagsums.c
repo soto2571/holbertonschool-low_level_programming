e05523ee31110f1f5a14ec09f1da823c5fddfa69
soto2571 <sebastian.soto1649@gmail.com>
2024-03-05 13:48:46 -0400

    estupida

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -4,18 +4,16 @@
  *print_diagsums - Start of the program
  *@a: array of integers
  *@size: size of the array
- *Return:
- *
  */
 void print_diagsums(int *a, int size)
 {
-	int add1 = 0;
-	int add2 = 0;
-	int i;
-	for (i = 0; i < size; i++)
-	{
-		add1 += *(a + i * size + i);
-		add2 += *(a + i * size + (size - 1 - i));
-	}
+int add1 = 0;
+int add2 = 0;
+int i;
+for (i = 0; i < size; i++)
+{
+	add1 += *(a + i * size + i);
+	add2 += *(a + i * size + (size - 1 - i));
+}
 	printf("%d, %d\n", add1, add2);
 }
