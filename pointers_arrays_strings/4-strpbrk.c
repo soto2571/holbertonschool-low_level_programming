1d2a8c7596490f713390438643b7ca0b5725830a
soto2571 <sebastian.soto1649@gmail.com>
2024-03-05 09:35:41 -0400

    jkfjfs

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,14 +3,24 @@
 /**
  *_strpbrk - Start of the program
  *@s:String
- *@accept:
+ *@accept:The set of byte
+ *Return:A pointer to a byte
  */
 char *_strpbrk(char *s, char *accept)
 {
+while (*s != '\0')
+{
+	char *currentAccept = accept;
 
-
-
-
-
-
+	while (*currentAccept != '\0')
+	{
+		if (*s == *currentAccept)
+		{
+			return (s);
+		}
+		currentAccept++;
+	}
+	s++;
+}
+return ((char *)0);
 }
