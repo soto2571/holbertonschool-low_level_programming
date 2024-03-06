30901deb4673b49481706352e39358afc38334ee
soto2571 <sebastian.soto1649@gmail.com>
2024-03-06 13:28:32 -0400

    ugghkh

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  *_strlen_recursion - Start of the program
- *
+ *Description: This code will return the string lenght
  *@s: Strin len
- *
+ *Return: The lenght of the string
  */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-	return 0;
+	return (0);
 }
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
