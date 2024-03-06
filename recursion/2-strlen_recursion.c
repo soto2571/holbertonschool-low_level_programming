4617259cf59b6ae5b13da343245fbd721e0b27e2
soto2571 <sebastian.soto1649@gmail.com>
2024-03-06 12:50:38 -0400

    skfjf

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen_recursion - Start of the program
+ *
+ *@s: Strin len
+ *
+ */
+int _strlen_recursion(char *s)
+{
+if (s == '\0')
+{
+	return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+}
