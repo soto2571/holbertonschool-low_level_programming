97df6efe54894e9bc99fa8081c127c2778ff664a
soto2571 <sebastian.soto1649@gmail.com>
2024-03-07 09:21:38 -0400

    fffew

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
+ *factorial - Start of the program
  *
- *
- *
- *
+ *@n: Value of number
+ *Return: Positive or negative
  */
-int factorial (int n)
+int factorial(int n)
 {
-if (n > 0)
+if (n < 0)
 {
-	return (n * factorial(n - 1));
+	return (-1);
+}
+else if (n == 0 || n == 1)
+{
+	return (1);
 }
 else
 {
-	return (-1);
+	return (n * factorial(n - 1));
 }
 }
