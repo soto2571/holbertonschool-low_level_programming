8acd7f9a5a325e340ccfbdf07cc0b3a148681d1e
soto2571 <sebastian.soto1649@gmail.com>
2024-03-07 10:05:55 -0400

    skljfdsjf

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_pow_recursion - Start of the program
+ *
+ *@x:The base
+ *
+ *@y:The exponent
+ *Return:Results
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+else if (y == 0)
+{
+	return (1);
+}
+else
+{
+	return (x * _pow_recursion(x, y - 1));
+}
+}
