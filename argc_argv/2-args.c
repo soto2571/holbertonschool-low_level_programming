93fd04f8b7d6e7f24a8d673556b9a64358e6e7c0
soto2571 <sebastian.soto1649@gmail.com>
2024-03-08 12:48:04 -0400

    slkfjfsj

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Start of the program
+ *@argc:Quantity of argument
+ *@argv:Argument array
+ *Return:0
+ */
+int main(int argc, char *argv)
+{
+int i;
+
+for (i = 0; i <= (argc - 1); i++)
+	printf("%s\n", argv[i]);
+return (0);
+}
