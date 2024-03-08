1fd03486970cdc0f3380b13b0cdeb78bfb5e42db
soto2571 <sebastian.soto1649@gmail.com>
2024-03-08 12:28:25 -0400

    slkfjsjkfsj

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  *main - start of the program
  *@argc:Count the lines
@@ -9,5 +9,5 @@
 int main(int argc, char *argv[])
 	(void)argv;
 	printf("%d\n", argc - 1);
-return (0);
+	return (0);
 }
