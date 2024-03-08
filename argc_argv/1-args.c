ebae9328b6ac3c7799f251ef67b2fd34829639ea
soto2571 <sebastian.soto1649@gmail.com>
2024-03-08 10:46:53 -0400

    skljfskfjs

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - start of the program
+ *@argc:Count the lines
+ *@argv:Count the strings
+ *Return:0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+
