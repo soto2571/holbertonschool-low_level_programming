81e767e7dcdac6454835426458900658558f7a39
soto2571 <sebastian.soto1649@gmail.com>
2024-03-08 13:42:48 -0400

    sfjskjf

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stlib.h>
+#include <ctype.h>
+/**
+ *main - Start of the program
+ *@argc: Number of argument
+ *@argv: Array with the program
+ *Return:0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i <= (argc - 1); i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			if (atoi(argv[i]) >= 0)
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
