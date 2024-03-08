53e9b3ce364f63b44548e25a830ad8b1da00faf9
soto2571 <sebastian.soto1649@gmail.com>
2024-03-08 13:16:58 -0400

    lkjfsjkfs

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,18 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
+int num1;
+int num2;
+
+if (argc != 3)
+{
 	printf("Error\n");
 	return (1);
-	}
+}
+else if (argc == 3)
+{
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	int result = num1 * num2;
-
-	printf("%d\n", result);
-
+	printf("%d\n", (num1 * num2));
 	return (0);
+}
 }
