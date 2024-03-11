36d5f3c305ed3accf13bab0ff136895c648c472d
soto2571 <sebastian.soto1649@gmail.com>
2024-03-11 12:12:58 -0400

    slkjffjs

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *array = (char *)malloc(size * sizeof(char));
-if (size == 0 || array == '\0')
+if (size == 0 || array == NULL)
 {
 	return (0);
 }
