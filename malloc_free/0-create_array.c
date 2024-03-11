1c1f40ea40725ae9061e4039530fd375e808691d
soto2571 <sebastian.soto1649@gmail.com>
2024-03-11 12:11:50 -0400

    skjfskjf

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,21 +10,15 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
-char *array;
-if (size == 0)
+char *array = (char *)malloc(size * sizeof(char));
+if (size == 0 || array == '\0')
 {
-	return (NULL);
-}
-
-char array = (char *)malloc(size * sizeof(char));
-
-if (array == NULL)
-{
-	return (NULL);
+	return (0);
 }
 for (i = 0; i < size; i++)
 {
 	array[i] = c;
 }
+array[i] = '\0';
 return (array);
 }
