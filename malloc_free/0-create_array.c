e83c9682e8397c4f47ecd44390d31196bcdb619f
soto2571 <sebastian.soto1649@gmail.com>
2024-03-11 11:52:44 -0400

    sljfsj

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *create_array - Start of the program
+ *@size: Size of bytes in the array
+ *@c:Char type
+ *Return:Returns a pointer to the array or NULL if its fails
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+if (size == 0)
+	return ('\0');
+}
+{
+char *array = (char *)malloc(size * sizeof(char));
+
+if (array == '\0')
+{
+	return ('\0');
+}
+for (i = 0; i < size; i++)
+{
+	array[i] = c;
+}
+
+return (array);
+
+}
