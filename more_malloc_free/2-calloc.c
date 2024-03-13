6525c155f69ca361d07666ffec6a109c2c38f8b9
soto2571 <sebastian.soto1649@gmail.com>
2024-03-13 12:54:57 -0400

    slkfsjfks

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stirng.h>
 /**
  *_calloc - allocate memory for an array and initialize with zeros
  *@nmemb: number of elements in the array
