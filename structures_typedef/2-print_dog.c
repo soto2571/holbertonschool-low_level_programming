952cc365bdefbcc82ccf897dbe29747d0f30f0df
soto2571 <sebastian.soto1649@gmail.com>
2024-03-14 14:21:11 -0400

    sdlfkjsjdf

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,11 +12,13 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-
-	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->Breed)
-		printf("Breed: %s\n", d->Breed);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
-		printf("Breed: (nil)\n");
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
