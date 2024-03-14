d5f46372a122342c43d7eee8ed207e813814f48f
soto2571 <sebastian.soto1649@gmail.com>
2024-03-14 10:19:13 -0400

    sklfskfl

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,16 @@
-#include <stdio.h>
-
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Struct dog
+ *@name: Name
+ *@age: Age
+ *@owner: Owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
