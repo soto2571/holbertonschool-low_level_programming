95d5724061f29bc107652e8ccc00d166dc4b0038
soto2571 <sebastian.soto1649@gmail.com>
2024-03-15 09:43:05 -0400

    sjffh

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,7 +11,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
