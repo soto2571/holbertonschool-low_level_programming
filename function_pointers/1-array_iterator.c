42a292b9d93280a647dd83c07e3d6a194977e0fb
soto2571 <sebastian.soto1649@gmail.com>
2024-03-18 11:02:10 -0400

    sjlkfsljf

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *array_iterator - iterates in the array
+ *@array: the array
+ *@size: the size of the array
+ *@action: a pointer to the fucntion
+ *
+ *
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+if (array == NULL || action == NULL)
+	return;
+while (size -- > 0)
+{
+	action(*array);
+	array++;
+}
+}
