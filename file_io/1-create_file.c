9ce0098a4a6a3f616f57a4070d268d05133eeb6d
soto2571 <sebastian.soto1649@gmail.com>
2024-04-04 13:36:38 -0400

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,25 +8,22 @@
 **/
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	int new_file, len, wr_stat;
 
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd == -1)
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
 		return (-1);
-
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(new_file);
+		return (1);
 	}
-	close(fd);
-	return (1);
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
 }
